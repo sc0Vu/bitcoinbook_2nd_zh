2ffa5b4e634bef674ddcf94ef4d21661f2d30ac5
awesome-doge <oknet821222@gms.tku.edu.tw>
2019-08-27 01:25:05 +0800

    更新

--- gitbook.sh
+++ gitbook.sh
@@ -2,11 +2,7 @@ git checkout master
 git add .
 git commit -m $1
 git push -u origin master
-git branch -d gh-pages
-git branch  -r -d  origin/gh-pages
-git push origin :gh-pages
-git checkout -b gh-pages
-git checkout  gh-pages
+git checkout gh-pages
 cp -r _book/* .
 git add .
 git commit -m $1
