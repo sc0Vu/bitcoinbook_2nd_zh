d894fd8e96a3a2fa20d2ceff19a718c0894c08fd
awesome-doge <oknet821222@gms.tku.edu.tw>
2019-08-27 02:37:23 +0800

    Update

--- gitbook.sh
+++ gitbook.sh
@@ -1,16 +1,6 @@
-git checkout master
+cd build
+git init
+git checkout -b gh-pages
 git add .
-git commit -m $1
-git push origin master
-git checkout --orphan gh-pages
-git rm --cached -r .
-git clean -df
-rm -rf *~
-echo "*~" > .gitignore
-echo "_book" >> .gitignore
-git add .gitignore
-git commit -m $1
-cp -r _book/* .
-git add .
-git commit -m "Publish book"
-git checkout master
+git commit -am "Update"
+git push  gh-pages --force
